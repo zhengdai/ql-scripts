13c9cb3e32d6351833d35228e571eb4385d4dfed
zhengdai <798484315@qq.com>
2024-05-06 22:27:41 +0800

    use pushplus

--- shejiao.js
+++ shejiao.js
@@ -1,8 +1,9 @@
-const key = 'SCT247069TNUXgDyRxxFvpAg0fPdk0BJU9';
-    //'SCT245797TQPmsilGTjKbWYDC4vAaVoHHh';
+const key = '2e18d8deb4744bfa846eb387981775a9';
+//'SCT247069TNUXgDyRxxFvpAg0fPdk0BJU9';
+//'SCT245797TQPmsilGTjKbWYDC4vAaVoHHh';
 
 const url = 'http://dtxtj.gdhuaxun.net:1009//App/DataApi.ashx';
-const push_url = `https://sctapi.ftqq.com/${key}.send`;
+const push_url = `http://www.pushplus.plus/send`;
 
 const loginData = {"Action":"login","UserName":"13923020981","Password":"2D49BE9704D22F8FCD8EF1F766E62C6B","PushID":"eb97cd8bb89843be","IMEI":"","DeviceModel":"TNA-AN00"};
 
@@ -21,7 +22,7 @@ const afternoonEnd = now.hour(17).minute(0).second(0).millisecond(0);
 const unsignList = [];
 const overstepList = [];
 
-const title = '打卡提醒\n\n';
+const title = '打卡提醒';
 
 let desp = '';
 
@@ -29,15 +30,20 @@ function isMorning() {
     return now.hour() < 12;
 }
 
-async function pushMsg(title, desp) {
-    const result = await axios.post(push_url, {
-        title,
-        desp,
-    });
-    console.log(result.data);
-}
 function isYidi(address) {
     return !address.includes('丰顺');
+}
+
+async function pushMsg(title, content) {
+    console.log("Pushplus 推送开始");
+    const data = {
+        token: key,
+        title,
+        content: content.replace("\n", "<br>"),
+        template: "json",
+    }
+    const res = await axios.post(push_url, data);
+    console.log(res.data);
 }
 
 const is_morning = isMorning();
@@ -46,7 +52,7 @@ const is_morning = isMorning();
     const loginResult = await axios.post(url, loginData);
     const loginDataResult = loginResult.data;
     if (loginDataResult.Code !== 1) {
-        desp += '登录失败\n\n';
+        desp += '登录失败 \r\n';
         await pushMsg(title, desp);
         return;
     }
@@ -55,7 +61,7 @@ const is_morning = isMorning();
     getSignRecord.Token = token;
     const userListResult = await axios.post(url, userListData);
     if (userListResult.data.Code !== 1) {
-        desp += '获取用户列表失败\n\n';
+        desp += '获取用户列表失败\r\n';
         await pushMsg(title, desp);
         return;
     }
@@ -66,7 +72,7 @@ const is_morning = isMorning();
         getSignRecord.UserName = user.UserName;
         const getSignRecordResult = await axios.post(url, getSignRecord);
         if (getSignRecordResult.data.Code !== 1) {
-            desp += `获取${user.TrueName} ${user.UserName}打卡列表失败\n\n`;
+            desp += `获取${user.TrueName} ${user.UserName}打卡列表失败 \r\n`;
             continue;
         }
         console.log(getSignRecordResult.data);
@@ -99,24 +105,25 @@ const is_morning = isMorning();
         }
     }
     if (unsignList.length > 0) {
-        desp += '未按时打卡人员：\n\n';
+        desp += '未按时打卡人员： \r\n';
         unsignList.forEach(item => {
             desp += `${item.TrueName} ${item.UserName} `;
             // if (item.signInTime) {
             //     desp += `上次打卡时间:${item.signInTime}`;
             // }
-            desp += '\n\n';
+            desp += ' \r\n';
         });
     } else {
-        desp += '所有人员都已按时打卡\n\n';
+        desp += '所有人员都已按时打卡 \r\n';
     }
     if (overstepList.length > 0) {
-        desp += '越界打卡人员：\n\n';
+        desp += '越界打卡人员： \r\n';
         overstepList.forEach(item => {
-            desp += `${item.TrueName} ${item.UserName}在${item.address}打卡\n\n`;
+            desp += `${item.TrueName} ${item.UserName}在${item.address}打卡 \r\n`;
         });
     }
 
     console.log(title, desp);
     await pushMsg(title, desp);
+
 })();
